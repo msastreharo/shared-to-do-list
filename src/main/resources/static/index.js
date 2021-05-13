669fc12fd18f11536c8aea5eb73aa7d4e3958c28
Marta SH <msastreharo@gmail.com>
2021-05-13 17:50:37 +0200

    function loadTasks() done

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -2,9 +2,10 @@ const tasksToDo = document.querySelector(".to-do-list");
 
 function loadTasks() {
     fetch("/tasks").then(res => res.json()).then(function (tasks) {
-        tasks.forEach(task => {
-            const element = `<div class="task">${task.description}</div>`;
+        tasks.forEach((task, i) => {
+            const element = `<div class="task" id="task-${i}">${task.description}</div>`;
             tasksToDo.insertAdjacentHTML("beforeend", element);
+            document.getElementById("task-${i}").addEventListener("click", deleteToDo())
         })
     })
 }
