a0ade1dd8d7cd450e4bdb6de5013429760e461f6
Marta SH <msastreharo@gmail.com>
2021-05-14 09:59:22 +0200

    Added fetch instructions

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,14 +1,21 @@
 const tasksToDo = document.querySelector(".to-do-list");
 
+function deleteToDo() {
+    console.log("todo tocado") ;
+    // Check fetch to delete: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+}
+
 function loadTasks() {
     fetch("/tasks").then(res => res.json()).then(function (tasks) {
         tasks.forEach((task, i) => {
-            const element = `<div class="task" id="task-${i}">${task.description}</div>`;
+            const element = `<div class="task" id="task-${i}"> ${task.description} </div>`;
             tasksToDo.insertAdjacentHTML("beforeend", element);
-            document.getElementById("task-${i}").addEventListener("click", deleteToDo())
+            document.getElementById(`task-${i}`).addEventListener("click", deleteToDo)
         })
     })
 }
+
+
 
 document.addEventListener("DOMContentLoaded", loadTasks);
 
